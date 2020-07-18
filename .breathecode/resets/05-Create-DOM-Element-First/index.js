965bc9e16d06b9ff9adcfb29f580742053e21146
George Mihov <gmihov001@gmail.com>
2020-07-18 20:00:44 +0000

    Updates

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,7 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-var aux = document.createElement("P");
-aux.innerHTML = "Hello World";
-aux.style.background = "yellow";
-document.querySelector("#myDiv").appendChild(aux);
--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,3 +1,5 @@
 //you code here
 document.body.innerHTML = "<h1>Hello World</h1>";
-
+document.body.innerHTML = "<img />";
+document.querySelector("img").id = "theImage";
+document.querySelector("#theImage").src = "http://placehold.it/350x150";
