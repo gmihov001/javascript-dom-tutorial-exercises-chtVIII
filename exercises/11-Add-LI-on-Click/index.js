664e9737bd674b21dea02cdd061a0bfed45b5d42
George Mihov <gmihov001@gmail.com>
2020-07-19 20:48:25 +0000

    Updates

--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -1,5 +1,7 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
-	//your code here
-
+	let list = document.querySelector("#myList");
+	let element = document.createElement("li");
+	element.innerHTML = "Fourth element";
+	list.appendChild(element);
 });
