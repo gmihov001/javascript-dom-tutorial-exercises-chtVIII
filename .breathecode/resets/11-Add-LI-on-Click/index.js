a4adae5d6256bf99f998dafe357948d0f2b79bcb
George Mihov <gmihov001@gmail.com>
2020-07-19 20:25:29 +0000

    Updates

--- .breathecode/resets/11-Add-LI-on-Click/index.js
+++ .breathecode/resets/11-Add-LI-on-Click/index.js
@@ -1,10 +1,5 @@
-let element = document.createElement('div');
-let text = document.createTextNode('Hello World');
-element.innerHTML = text;
-
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
 
 });
