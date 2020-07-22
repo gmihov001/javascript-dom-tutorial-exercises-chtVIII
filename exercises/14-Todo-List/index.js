a6e5347fb005bdb84f09262765c7362a8ab80c81
George Mihov <george.mihov@forwardlane.com>
2020-07-22 07:10:07 -0400

    Updates

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+function deleteTask() {
+	let elements = document.querySelectorAll(".fa-trash");
+	console.log(elements);
+	elements.forEach(item => {
+		item.addEventListener("click", function(e) {
+			let deletedItem = e.target.parentNode.parentNode;
+			deletedItem.parentNode.removeChild(deletedItem);
+		});
+	});
+}
+
+window.addTask = function addTask(event) {
+	if (event.keyCode == 13) {
+		let newTask = event.target.value;
+		let newEntry = document.createElement("li");
+		newEntry.innerHTML = `<span><i class="fa fa-trash"></i></span> ${newTask}`;
+		document.querySelector("ul").appendChild(newEntry);
+		document.querySelector("#addToDo").value = null;
+		deleteTask();
+	}
+};
+
+deleteTask();
